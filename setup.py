e6a96eb48175566ecc27546742652741857c1ff7
Antoine Millet <antoine@inaps.org>
2012-04-14 23:01:07 +0200

    Started development on 1.6~dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.5'
+version = '1.6~dev'
 ldesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(name='dotconf',
