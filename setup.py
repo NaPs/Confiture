62657afb048c858c43a7a294e811c7410caf7580
Antoine Millet <antoine@inaps.org>
2013-09-08 22:22:51 +0200

    Started development on 1.9~dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.8'
+version = '1.9~dev'
 ldesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(name='dotconf',
